563e52fb160d397f12fae60126f93153d896f47b
Yuri Dotsya <yuriy.dotsya@gmail.com>
2017-12-25 22:04:41 +0300

    routes tests for vacancies

--- vacancies/tests.py
+++ vacancies/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from users.models import User
 
-# Create your tests here.
+
+class VacanciesRoutesTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        user = User.objects.create_user('user', 'user@gmail.com', '123456Test')
+        user.is_employer = False
+        user.save()
+        employer = User.objects.create_user('employer', 'employer@gmail.com', '123456Test')
+        employer.is_employer = True
+        employer.save()
+
+    def test_user_can_add_vacancy(self):
+        self.client.login(username='user', password='123456Test')
+        response = self.client.get('/vacancies/add-vacancy/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_employer_can_add_vacancy(self):
+        self.client.login(username='employer', password='123456Test')
+        response = self.client.get('/vacancies/add-vacancy/')
+        self.assertEqual(response.status_code, 200)
